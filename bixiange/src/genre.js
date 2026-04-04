505899f0eef45c68e884ec9477d6dbd2c3c26f90
hienpro00123 <hienmeoukom@gmail.com>
2026-04-04 22:01:06 +0700

    feat: bixiange translate all tabs/genres to Vietnamese

--- bixiange/src/genre.js
+++ bixiange/src/genre.js
@@ -1,18 +1,18 @@
-load("config.js");
+﻿load("config.js");
 
 function execute() {
     return Response.success([
-        { title: "\u90fd\u5e02\u8a00\u60c5", input: "dsyq",      script: "genrecontent.js" },
-        { title: "\u6b66\u4fa0\u4fee\u771f", input: "wxxz",      script: "genrecontent.js" },
-        { title: "\u7384\u5e7b\u5947\u5e7b", input: "xhqh",      script: "genrecontent.js" },
-        { title: "\u7a7f\u8d8a\u67b6\u7a7a", input: "cyjk",      script: "genrecontent.js" },
-        { title: "\u79d1\u5e7b\u7ade\u6280", input: "khjj",      script: "genrecontent.js" },
-        { title: "\u9b3c\u8bdd\u60ac\u7591", input: "ghxy",      script: "genrecontent.js" },
-        { title: "\u519b\u4e8b\u5386\u53f2", input: "jsls",      script: "genrecontent.js" },
-        { title: "\u5b98\u573a\u5546\u6218", input: "guanchang", script: "genrecontent.js" },
-        { title: "\u4e61\u571f\u98ce\u60c5", input: "xtfq",      script: "genrecontent.js" },
-        { title: "\u803b\u7f8e\u5c0f\u8bf4", input: "dmtr",      script: "genrecontent.js" },
-        { title: "\u540c\u4eba\u5c0f\u8bf4", input: "trxs",      script: "genrecontent.js" },
-        { title: "\u7cbe\u54c1\u5c0f\u8bf4", input: "jqxs",      script: "genrecontent.js" },
+        { title: "Ngôn tình đô thị",       input: "dsyq",      script: "genrecontent.js" },
+        { title: "Võ hiệp tu tiên",         input: "wxxz",      script: "genrecontent.js" },
+        { title: "Huyền hư kỳ ảo",         input: "xhqh",      script: "genrecontent.js" },
+        { title: "Xuyên không dị đại",      input: "cyjk",      script: "genrecontent.js" },
+        { title: "Khoa viễn cạnh tranh",    input: "khjj",      script: "genrecontent.js" },
+        { title: "Ma quỷ kinh dị",          input: "ghxy",      script: "genrecontent.js" },
+        { title: "Quân sự lịch sử",         input: "jsls",      script: "genrecontent.js" },
+        { title: "Chính trường thương chiến", input: "guanchang", script: "genrecontent.js" },
+        { title: "Thôn quê phong tục",      input: "xtfq",      script: "genrecontent.js" },
+        { title: "Truyện BL",               input: "dmtr",      script: "genrecontent.js" },
+        { title: "Fan fiction",             input: "trxs",      script: "genrecontent.js" },
+        { title: "Tinh phẩm",               input: "jqxs",      script: "genrecontent.js" },
     ]);
 }
--- bixiange/src/home.js
+++ bixiange/src/home.js
@@ -1,9 +1,9 @@
-load("config.js");
+﻿load("config.js");
 
 function execute() {
     return Response.success([
-        { title: "\u6700\u65b0", input: "newest", script: "homecontent.js" },
-        { title: "\u70ed\u95e8", input: "sort",   script: "homecontent.js" },
-        { title: "\u63a8\u8350", input: "home",   script: "homecontent.js" },
+        { title: "Mới nhất", input: "newest", script: "homecontent.js" },
+        { title: "Nổi bật",  input: "sort",   script: "homecontent.js" },
+        { title: "Đề xuất",  input: "home",   script: "homecontent.js" },
     ]);
 }
