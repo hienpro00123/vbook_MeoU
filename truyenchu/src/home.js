031a04e3345c6c4af1798161ef5a72608cd224d9
hienpro00123 <hienmeoukom@gmail.com>
2026-04-05 21:24:10 +0700

    TruyenChu v1: fix genre.js and home.js use title+script (not name)

--- truyenchu/src/home.js
+++ truyenchu/src/home.js
@@ -2,7 +2,7 @@ load("config.js");
 
 function execute() {
     return Response.success([
-        { name: "Mới Cập Nhật", input: "truyen", script: "homecontent.js" },
-        { name: "Tìm Theo Thể Loại", input: "", script: "genre.js" }
+        { title: "Mới Cập Nhật",       input: "truyen", script: "homecontent.js" },
+        { title: "Tìm Theo Thể Loại",  input: "",       script: "genre.js" }
     ]);
 }
