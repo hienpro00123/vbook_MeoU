ecd24d62ee01e4719c30ba5319f1cfcd07e1dfaa
hienpro00123 <hienmeoukom@gmail.com>
2026-04-06 21:35:18 +0700

    feat(truyenchu): restore genre tab

--- truyenchu/src/home.js
+++ truyenchu/src/home.js
@@ -2,6 +2,7 @@
 
 function execute() {
     return Response.success([
-        { title: "Mới Cập Nhật", input: "truyen", script: "homecontent.js" }
+        { title: "Mới Cập Nhật",      input: "truyen", script: "homecontent.js" },
+        { title: "Tìm Theo Thể Loại", input: "",       script: "genre.js" }
     ]);
 }
