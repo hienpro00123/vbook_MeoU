36de006c442dcff808272df623034ef51fc27471
hienpro00123 <hienmeoukom@gmail.com>
2026-04-06 21:37:11 +0700

    fix(truyenchu): remove Tìm Theo Thể Loại tab from home, keep genre script

--- truyenchu/src/home.js
+++ truyenchu/src/home.js
@@ -2,7 +2,6 @@
 
 function execute() {
     return Response.success([
-        { title: "Mới Cập Nhật",      input: "truyen", script: "homecontent.js" },
-        { title: "Tìm Theo Thể Loại", input: "",       script: "genre.js" }
+        { title: "Mới Cập Nhật", input: "truyen", script: "homecontent.js" }
     ]);
 }
