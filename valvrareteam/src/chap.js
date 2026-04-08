945a2c8907af653e2503cf032e0fb0461e20d205
hienpro00123 <hienmeoukom@gmail.com>
2026-04-08 22:04:13 +0700

    valvrareteam v8: dung fetch+Jsoup thay WebView cho chap.js (nhanh hon)

--- valvrareteam/src/chap.js
+++ valvrareteam/src/chap.js
@@ -6,19 +6,14 @@ function execute(url) {
     if (!chapId || !novelId) return Response.error("URL chương không hợp lệ");
 
     var webUrl = BASE_URL + "/truyen/x-" + shortId(novelId) + "/chuong/" + shortId(chapId);
-    var browser = Engine.newBrowser();
-    try {
-        var doc = browser.launch(webUrl, 15000);
-        if (doc) {
-            var el = doc.selectFirst(".chapter-content");
-            if (el) {
-                el.select("script, style, noscript, iframe").remove();
-                return Response.success(el.html());
-            }
+    var res = fetch(webUrl);
+    if (res && res.ok) {
+        var doc = Response.html(res.body);
+        var el = doc.selectFirst(".chapter-content");
+        if (el) {
+            el.select("script, style, noscript, iframe").remove();
+            return Response.success(el.html());
         }
-    } catch(e) {
-    } finally {
-        try { browser.close(); } catch(e2) {}
     }
     return Response.error("Không tải được nội dung chương");
 }
