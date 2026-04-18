b1f383d35d256748779ca9f2fce578e9f15e9ff3
hienpro00123 <hienmeoukom@gmail.com>
2026-04-18 12:30:03 +0700

    loppytoon v10: fix comic chap return HTML img tags for novel reader

--- loppytoon/src/chap.js
+++ loppytoon/src/chap.js
@@ -24,15 +24,15 @@ function execute(url) {
     var imgs = doc.select(".chapter-content .manga-images-container img, .chapter-content img.manga-image");
     if (imgs.size() === 0) return Response.error("Khong co anh trong chuong");
 
-    var data = [];
+    var htmlParts = [];
     for (var i = 0; i < imgs.size(); i++) {
         var img = imgs.get(i);
         var src = img.attr("src") || img.attr("data-src") || "";
         if (!src) continue;
         if (src.indexOf("http") !== 0) src = STORAGE_URL + src;
-        data.push({ link: src });
+        htmlParts.push('<img src="' + src + '" style="width:100%;display:block">');
     }
 
-    if (data.length === 0) return Response.error("Khong co anh trong chuong");
-    return Response.success(data);
+    if (htmlParts.length === 0) return Response.error("Khong co anh trong chuong");
+    return Response.success(htmlParts.join(""));
 }
