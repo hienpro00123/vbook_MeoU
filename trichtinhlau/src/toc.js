afabfc0ce0d51c5b55bb22c0d2fadbd6428dd395
hienpro00123 <hienmeoukom@gmail.com>
2026-04-19 12:38:29 +0700

    trichtinhlau v5: toc load full pagination

--- trichtinhlau/src/toc.js
+++ trichtinhlau/src/toc.js
@@ -2,25 +2,39 @@ load("config.js");
 
 function execute(url) {
     var storyUrl = resolveUrl(url);
-    var res = fetchRetry(storyUrl);
-    if (!res || !res.ok) return Response.error("Không tải được mục lục");
-    var doc = res.html();
-    if (!doc) return Response.error("Không tải được mục lục");
+    var baseUrl = storyUrl.replace(/\?.*$/, "");
 
     var chapters = [];
     var seen = {};
+    var page = 1;
 
-    var chapLinks = doc.select("a[href*='/doc-truyen/']");
-    for (var i = 0; i < chapLinks.size(); i++) {
-        var a = chapLinks.get(i);
-        var href = a.attr("href");
-        if (!href || seen[href]) continue;
-        seen[href] = true;
-        var name = a.text().trim();
-        if (!name) continue;
-        var fullUrl = href.indexOf("http") === 0 ? href : BASE_URL + href;
-        chapters.push({ name: name, url: fullUrl, host: HOST });
+    while (true) {
+        var fetchUrl = page === 1 ? baseUrl : (baseUrl + "?page=" + page);
+        var res = fetchRetry(fetchUrl);
+        if (!res || !res.ok) break;
+        var doc = res.html();
+        if (!doc) break;
+
+        var chapLinks = doc.select("a[href*='/doc-truyen/']");
+        var added = 0;
+        for (var i = 0; i < chapLinks.size(); i++) {
+            var a = chapLinks.get(i);
+            var href = a.attr("href");
+            if (!href || seen[href]) continue;
+            seen[href] = true;
+            var name = a.text().trim();
+            if (!name) continue;
+            var fullUrl = href.indexOf("http") === 0 ? href : BASE_URL + href;
+            chapters.push({ name: name, url: fullUrl, host: HOST });
+            added++;
+        }
+
+        // Kiểm tra có trang tiếp theo không
+        var nextLink = selFirst(doc, "a[href*='?page=" + (page + 1) + "']");
+        if (!nextLink || added === 0) break;
+        page++;
     }
 
+    if (chapters.length === 0) return Response.error("Không tìm thấy danh sách chương");
     return Response.success(chapters);
 }
