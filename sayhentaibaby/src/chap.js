44cab8235a0a480141f1c7234e89af1e2f04566b
hienpro00123 <hienmeoukom@gmail.com>
2026-04-21 21:26:13 +0700

    sayhentaibaby v13: doi sang sv3 DuckDuckGo proxy lam primary de tranh loi Android CDN

--- sayhentaibaby/src/chap.js
+++ sayhentaibaby/src/chap.js
@@ -16,11 +16,15 @@ function execute(url) {
     var data = [];
     for (var i = 0; i < imgs.size(); i++) {
         var img = imgs.get(i);
-        // sv2 = i0.wp.com proxy (IPv4, on dinhh) - uu tien sv2 tranh loi IPv6 CDN
-        var sv2 = img.attr("data-sv2") || "";
         var sv1 = img.attr("src") || img.attr("data-sv1") || "";
+        var sv2 = img.attr("data-sv2") || "";
         var sv3 = img.attr("data-sv3") || "";
-        var src = sv2 || sv1 || sv3;
+        // Fallback: tu construct DuckDuckGo proxy neu data-sv3 trong
+        if (!sv3 && sv1 && sv1.indexOf("http") === 0) {
+            try { sv3 = "https://external-content.duckduckgo.com/iu/?u=" + encodeURIComponent(sv1); } catch (e) { sv3 = ""; }
+        }
+        // sv3 (DuckDuckGo IPv4 proxy) > sv1 > sv2
+        var src = sv3 || sv1 || sv2;
         if (!src || src.indexOf("data:image") === 0) continue;
         data.push({ link: src });
     }
