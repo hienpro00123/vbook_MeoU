e8faa9cd92a610209cf7aef7e121d85282acf1a0
hienpro00123 <hienmeoukom@gmail.com>
2026-04-21 21:01:36 +0700

    sayhentaibaby v11: server fallback sv1->sv2->sv3 in chap.js

--- sayhentaibaby/src/chap.js
+++ sayhentaibaby/src/chap.js
@@ -1,5 +1,15 @@
 load("config.js");
 
+function testUrl(url) {
+    if (!url) return false;
+    try {
+        var res = fetch(url, FETCH_OPTIONS);
+        return res != null && res.ok;
+    } catch (e) {
+        return false;
+    }
+}
+
 function execute(url) {
     var chapUrl = url.indexOf("http") === 0 ? url : resolveUrl(url);
     var res = fetchRetry(chapUrl);
@@ -10,16 +20,41 @@ function execute(url) {
     // div.contentimg > div.imageload > img.simg[src]
     var imgs = doc.select("div.contentimg img.simg");
     if (imgs.size() === 0) {
-        // fallback: all img trong contentimg
         imgs = doc.select("div.contentimg img");
     }
     if (imgs.size() === 0) return Response.error("Khong co anh trong chuong");
 
-    var data = [];
+    // Thu thap URL cac server cho tung anh
+    var sv1List = [];
+    var sv2List = [];
+    var sv3List = [];
     for (var i = 0; i < imgs.size(); i++) {
         var img = imgs.get(i);
-        // src trực tiếp, data-sv1 là backup cùng CDN
-        var src = img.attr("src") || img.attr("data-sv1") || "";
+        sv1List.push(img.attr("src") || img.attr("data-sv1") || "");
+        sv2List.push(img.attr("data-sv2") || "");
+        sv3List.push(img.attr("data-sv3") || "");
+    }
+
+    // Phat hien server tot nhat bang cach test anh dau tien
+    var useServer = 1;
+    if (!testUrl(sv1List[0])) {
+        if (testUrl(sv2List[0])) {
+            useServer = 2;
+        } else if (testUrl(sv3List[0])) {
+            useServer = 3;
+        }
+    }
+
+    var data = [];
+    for (var j = 0; j < sv1List.length; j++) {
+        var src;
+        if (useServer === 2 && sv2List[j]) {
+            src = sv2List[j];
+        } else if (useServer === 3 && sv3List[j]) {
+            src = sv3List[j];
+        } else {
+            src = sv1List[j];
+        }
         if (!src || src.indexOf("data:image") === 0) continue;
         data.push({ link: src });
     }
