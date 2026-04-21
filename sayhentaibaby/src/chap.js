4c49f0e2d9c1f51d9f4a81bf8ef664fcdbc4a8ba
hienpro00123 <hienmeoukom@gmail.com>
2026-04-21 21:12:08 +0700

    sayhentaibaby v12: prefer sv2 (wp.com IPv4) over sv1 (Cloudflare IPv6 fail)

--- sayhentaibaby/src/chap.js
+++ sayhentaibaby/src/chap.js
@@ -1,15 +1,5 @@
 load("config.js");
 
-function testUrl(url) {
-    if (!url) return false;
-    try {
-        var res = fetch(url, FETCH_OPTIONS);
-        return res != null && res.ok;
-    } catch (e) {
-        return false;
-    }
-}
-
 function execute(url) {
     var chapUrl = url.indexOf("http") === 0 ? url : resolveUrl(url);
     var res = fetchRetry(chapUrl);
@@ -17,44 +7,20 @@ function execute(url) {
     var doc = res.html();
     if (!doc) return Response.error("Khong doc duoc noi dung chuong");
 
-    // div.contentimg > div.imageload > img.simg[src]
     var imgs = doc.select("div.contentimg img.simg");
     if (imgs.size() === 0) {
         imgs = doc.select("div.contentimg img");
     }
     if (imgs.size() === 0) return Response.error("Khong co anh trong chuong");
 
-    // Thu thap URL cac server cho tung anh
-    var sv1List = [];
-    var sv2List = [];
-    var sv3List = [];
+    var data = [];
     for (var i = 0; i < imgs.size(); i++) {
         var img = imgs.get(i);
-        sv1List.push(img.attr("src") || img.attr("data-sv1") || "");
-        sv2List.push(img.attr("data-sv2") || "");
-        sv3List.push(img.attr("data-sv3") || "");
-    }
-
-    // Phat hien server tot nhat bang cach test anh dau tien
-    var useServer = 1;
-    if (!testUrl(sv1List[0])) {
-        if (testUrl(sv2List[0])) {
-            useServer = 2;
-        } else if (testUrl(sv3List[0])) {
-            useServer = 3;
-        }
-    }
-
-    var data = [];
-    for (var j = 0; j < sv1List.length; j++) {
-        var src;
-        if (useServer === 2 && sv2List[j]) {
-            src = sv2List[j];
-        } else if (useServer === 3 && sv3List[j]) {
-            src = sv3List[j];
-        } else {
-            src = sv1List[j];
-        }
+        // sv2 = i0.wp.com proxy (IPv4, on dinhh) - uu tien sv2 tranh loi IPv6 CDN
+        var sv2 = img.attr("data-sv2") || "";
+        var sv1 = img.attr("src") || img.attr("data-sv1") || "";
+        var sv3 = img.attr("data-sv3") || "";
+        var src = sv2 || sv1 || sv3;
         if (!src || src.indexOf("data:image") === 0) continue;
         data.push({ link: src });
     }
